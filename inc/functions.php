a079ed2c4630c146120eaa5c52700496029d2971
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-07-12 00:20:21 +0700

    Update autoload paths in composer.json (#36)

---
+++ inc/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/app/Str/CaseConverter/functions.php';
+require_once dirname(__DIR__) . '/app/Str/Inflector/functions.php';
+require_once dirname(__DIR__) . '/app/Validator/functions.php';
